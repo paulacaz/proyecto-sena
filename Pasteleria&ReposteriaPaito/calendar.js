f7839e276730ac43fd0e9506b695a512cf0f481d
Maria Paula Cabrera <mpaulaazu@gmail.com>
2024-05-20 20:26:34 -0500

    version 20-05-2024

--- Pasteleria&ReposteriaPaito/calendar.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const days = document.querySelectorAll('.day');
-
-    days.forEach(day => {
-        day.addEventListener('click', function() {
-            alert('Día seleccionado: ' + this.textContent);
-        });
-    });
-});
